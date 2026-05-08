8b36adbabf2546debe8c08e2fe603f330b213720
João Vitor Barreto <barretojoao440@gmail.com>
2026-05-08 20:00:51 -0300

    chore: create dopamine focus project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main>
+      <h1>Dopamine Focus</h1>
+      <p>Aplicativo gamificado para foco, tarefas e organização.</p>
+    </main>
+  );
+}
+
+export default App;
